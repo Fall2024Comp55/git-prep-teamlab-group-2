068ff3f153fbc37b43af8c2f29745c5387f3fa6b
TYTengYuan <t_yuan2@u.pacific.edu>
2025-02-14 16:46:17 -0800

    change the text to great

--- GitTeamPrep/src/SimplePicture.java
+++ GitTeamPrep/src/SimplePicture.java
@@ -16,7 +16,7 @@ public class SimplePicture extends GraphicsProgram {
 	public void run() {
 		GImage robot = new GImage(FILE_NAME, 200, 100);
 		add(robot);
-		GLabel label = new GLabel("How I feel when using github hackslash", 200, 400);
+		GLabel label = new GLabel("Great", 200, 400);
 		add(label);
 	}
 
